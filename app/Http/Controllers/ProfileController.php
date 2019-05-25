ad956670e64f3ca1fbea279f622d353659b81895
unknown <paulusw.94@gmail.com>
2019-05-25 14:23:24 +0700

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -16,4 +17,17 @@ class ProfileController extends Controller
     {
         $this->middleware('auth');
     }
+
+	function profile(Request $request) {
+		$input = $request->only(['name', 'telephone']);
+		$validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'telephone' => ['required', 'string', 'max:255']
+        ])->validate();
+
+		$user_data = User::find(Auth::id());
+		$user_data->name = $input->name;
+		$user_data->telephone = $input->telephone;
+		$user_data->save();
+	}
 }
