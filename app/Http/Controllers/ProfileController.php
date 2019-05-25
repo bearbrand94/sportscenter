3eac70d20885c084ffeb15995f15c4fd34e23823
unknown <paulusw.94@gmail.com>
2019-05-25 14:32:05 +0700

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,8 +18,21 @@ class ProfileController extends Controller
         $this->middleware('auth');
     }
 
-	function profile(Request $request) {
+	function update_profile(Request $request) {
 		$input = $request->only(['name', 'telephone']);
+		$validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'telephone' => ['required', 'string', 'max:255']
+        ])->validate();
+
+		$user_data = User::find(Auth::id());
+		$user_data->name = $input->name;
+		$user_data->telephone = $input->telephone;
+		$user_data->save();
+	}
+
+	function change_password(Request $request) {
+		$input = $request->only(['old_password', 'new_password']);
 		$validator = Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'telephone' => ['required', 'string', 'max:255']
